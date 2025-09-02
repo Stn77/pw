f414b0e0a45f9cbfea4747f02b7a7dafcfd26b52
Stn77 <fudinyusuf20@gmail.com>
2025-09-02 08:51:35 +0700

    Refactor dashboard and welcome views to include title attributes and improve layout structure

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,3 +1,3 @@
-<x-layouts.app>
+<x-layouts.app title="Dashboard" pageTitleName="Dashboard">
     login success
 </x-layouts.app>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,19 @@
-<x-layouts.app title="Welcome">
-    <p class="text-danger fs-1 bg-danger bg-opacity-25 px-4 rounded">Welcome</p>
-    <a href="{{route('login')}}" class="btn btn-primary">
-        Login
-    </a>
+<x-layouts.app pageTitleName="" sidebarShow=''>
+    @push('css')
+    <style>
+        .welcome-main{
+            min-height: 100vh;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+    </style>
+    @endpush
+    <div class="welcome-main">
+        <p class="text-danger fs-1 bg-danger bg-opacity-25 px-4 rounded">Welcome</p>
+        <a href="{{route('login')}}" class="btn btn-primary">
+            Login
+        </a>
+    </div>
 </x-layouts.app>
