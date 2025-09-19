b1e416a42ab8e23891ce635ed2c128128f30dc71
Stn77 <fudinyusuf20@gmail.com>
2025-09-19 16:32:34 +0700

    remove email field from fillable attributes and migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,7 +20,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'email',
+        // 'email',
         'password',
         'username'
     ];
@@ -57,7 +57,7 @@ class User extends Authenticatable
     {
         return $this->hasMany(RiwayatAbsen::class);
     }
-    
+
     public function guru()
     {
         return $this->hasOne(Guru::class);
