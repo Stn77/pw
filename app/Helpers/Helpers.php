3686186cf4aa6c68f25528ff3c4ec049af494017
Stn77 <fudinyusuf20@gmail.com>
2025-09-21 11:07:35 +0700

    feat: add autoloading for Helpers.php and ensure random string generation function is available

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -1,10 +1,10 @@
 <?php
 
-// if(!function_exists('getCurrentUser')) {
-//     function getCurrentUser() {
-//         return auth()->user();
-//     }
-// }
+if(!function_exists('generateRandomString')) {
+    function generateRandomString($length = 10) {
+        return substr(str_shuffle(str_repeat('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length / 62))), 1, $length);
+    }
+}
 
 if(!function_exists('createQrCode')) {
     function createQrCode($text, $size = 200) {
