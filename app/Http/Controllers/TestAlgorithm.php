91fbcdbf9081719aef0c701762f3862ce0098d91
Stn77 <fudinyusuf20@gmail.com>
2025-10-03 07:38:03 +0700

    new algo test

--- app/Http/Controllers/TestAlgorithm.php
+++ app/Http/Controllers/TestAlgorithm.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\GuruPivot;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -9,11 +10,36 @@ class TestAlgorithm extends Controller
 {
     public function index()
     {
-        dd($this->getTeacher());
+        $this->getKelasJurusanGuru();
     }
 
     function getTeacher()
     {
         return User::role('teacher')->get();
+    }
+
+    function getKelasJurusanGuru()
+    {
+        $datas = GuruPivot::with('kelas', 'jurusan', 'guru')->where('guru_id', 1)->get();
+        $kelasDatas = [];
+        $kelasDataIds = [];
+
+        for ($i = 0; $i <= $datas->count()-1;){
+            $kelasDatasContainer = $datas[$i]->kelas->name . ' ' . $datas[$i]->jurusan->name;
+            $kelasDatas[] = strtoupper($kelasDatasContainer);
+            $kelasDataIds[] = $datas[$i]->id;
+            $i++;
+        }
+
+        dd($kelasDatas, $kelasDataIds);
+
+        return $kelasDatas;
+    }
+
+    function getIdKelasJurusanGuru()
+    {
+        $datas = GuruPivot::where('guru_id', 1)->get();
+
+        dd($datas->kelas_id);
     }
 }
